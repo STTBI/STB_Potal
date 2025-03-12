15ecc50ff33920a19e95871668f0c93af5cc5e8f
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-03-13 01:39:10 +0900

    Chore: 캐릭터 카메라 위치 조정
    위치조정

--- Assets/02.Scripts/Player/PlayerController.cs
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -11,10 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float playerSpeed = 2.0f;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
+    private InputManager inputManager;
 
     private void OnValidate()
     {
         controller = GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
     }
 
     void Update()
@@ -25,7 +25,8 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
@@ -34,7 +35,7 @@ public class PlayerController : MonoBehaviour
         }
 
         // Makes the player jump
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
---
+++ Assets/02.Scripts/Singleton/InputManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+    private Player_AC playerAC;
+
+    private void Awake()
+    {
+        playerAC = new Player_AC();
+    }
+
+    private void OnEnable()
+    {
+        playerAC.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerAC.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerAC.Player.Movement.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMouseDelta()
+    {
+        return playerAC.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool PlayerJumpedThisFrame()
+    {
+        return playerAC.Player.Jump.triggered;
+    }
+}
