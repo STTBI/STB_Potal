4592083152709fc31cbefb1a78be888bbb39486c
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-03-12 16:50:13 +0900

    Asset
    포탈 에셋 추가

--- Assets/PortalCamera.cs
+++ Assets/PortalCamera.cs
@@ -30,9 +30,9 @@ public class PortalCamera : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
